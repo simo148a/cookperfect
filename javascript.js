26b8e34c86cae2d2eeb3a089fd38c5081a4b1f87
Anja Holst Nielsen <anjaholstnielsen@Anjas-MacBook-Pro-2.local>
2018-12-03 21:35:18 +0100

    Anja retter

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
-
-const sections = document.querySelectorAll("section");
+/*const sections = document.querySelectorAll("section");
 console.log(sections);
 window.addEventListener("scroll", function(){
   sections.forEach(section => {
@@ -12,4 +11,33 @@ window.addEventListener("scroll", function(){
       section.classList.remove('active');
     }
   });
-})
+})*/
+
+
+
+
+
+
+//$window.trigger('scroll');
+/*$element.animate({
+  marginLeft: 100
+}, "slow").fadeIn("slow", 1);*/
+
+
+
+/*
+
+
+$.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) $element.addClass('hasSlid');
+    else $element.removeClass('hasSlid');
+  });
+
+
+
+*/
