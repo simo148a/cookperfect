e5b7702f43cdb056dd8d36ca98afd5ec066c625a
Ida Lygum Pedersen <idalygumpedersen@Idas-MacBook-Pro-3.local>
2018-12-11 14:32:34 +0100

    sprog

--- javascript.js
+++ javascript.js
@@ -1,41 +1,41 @@
 //Menu scroll
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-100px";
-    }
-    prevScrollpos = currentScrollPos;
+	var currentScrollPos = window.pageYOffset;
+	if (prevScrollpos > currentScrollPos) {
+		document.getElementById("navbar").style.top = "0";
+	} else {
+		document.getElementById("navbar").style.top = "-100px";
+	}
+	prevScrollpos = currentScrollPos;
 }
 
 
 
 //Smooth scrolling
 $(document).ready(function () {
-    // Add smooth scrolling to all links
-    $("a").on('click', function (event) {
+	// Add smooth scrolling to all links
+	$("a").on('click', function (event) {
 
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
 
-            // Store hash
-            var hash = this.hash;
+			// Store hash
+			var hash = this.hash;
 
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 1000, function () {
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 1000, function () {
 
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
-    });
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			});
+		} // End if
+	});
 });
 
 //close when click
@@ -52,54 +52,54 @@ $(document).ready(function () {
 //Symboler fader ind
 $(document).ready(function () {
 
-    /* Every time the window is scrolled ... */
-    $(window).scroll(function () {
+	/* Every time the window is scrolled ... */
+	$(window).scroll(function () {
 
-        /* Check the location of each desired element */
-        $('.fadeinleft').each(function (i) {
+		/* Check the location of each desired element */
+		$('.fadeinleft').each(function (i) {
 
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            /* If the object is completely visible in the window, fade it it */
-            if (bottom_of_window > bottom_of_object) {
+			/* If the object is completely visible in the window, fade it it */
+			if (bottom_of_window > bottom_of_object) {
 
-                $(this).animate({
-                    'opacity': '1',
-                    'left': '0'
-                }, 800);
+				$(this).animate({
+					'opacity': '1',
+					'left': '0'
+				}, 800);
 
-            }
+			}
 
-        });
+		});
 
-    });
+	});
 
 });
 
 $(document).ready(function () {
 
-    /* Every time the window is scrolled ... */
-    $(window).scroll(function () {
+	/* Every time the window is scrolled ... */
+	$(window).scroll(function () {
 
-        /* Check the location of each desired element */
-        $('.fadeinright').each(function (i) {
+		/* Check the location of each desired element */
+		$('.fadeinright').each(function (i) {
 
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            /* If the object is completely visible in the window, fade it it */
-            if (bottom_of_window > bottom_of_object) {
+			/* If the object is completely visible in the window, fade it it */
+			if (bottom_of_window > bottom_of_object) {
 
-                $(this).animate({
-                    'opacity': '1',
-                    'left': '0'
-                }, 800);
+				$(this).animate({
+					'opacity': '1',
+					'left': '0'
+				}, 800);
 
-            }
+			}
 
-        });
+		});
 
-    });
+	});
 
 });
